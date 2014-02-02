3dbd6c3483df989bb4a91bcdd3014416844217a6
server <server@ubuntu.ubuntu-domain>
2014-02-02 14:18:10 +0200

    intial commit

---
+++ src/test/java/wad/easypoints/EasyPointsTest.java
@@ -0,0 +1,13 @@
+package wad.easypoints;
+
+import fi.helsinki.cs.tmc.edutestutils.Points;
+import org.junit.Test;
+
+@Points("W1E01")
+public class EasyPointsTest {
+
+    @Test
+    public void nothingToTest() {
+        // you shall pass!
+    }
+}
